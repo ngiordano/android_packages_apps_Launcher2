dd62d1e4454aab922959a02ac58478b53b2be1db
codenamedroid <codenamegit@gmail.com>
2012-01-12 21:13:14 -0600

    Launcher : Grid size (Danesh M)

--- src/com/android/launcher2/preference/PreferencesProvider.java
+++ src/com/android/launcher2/preference/PreferencesProvider.java
@@ -65,6 +65,14 @@ public final class PreferencesProvider {
                     return preferences.getBoolean("ui_homescreen_indicator_fade", true);
                 }
             }
+            public static int getGridRowCount(Context context) {
+                final SharedPreferences preferences = context.getSharedPreferences(PREFERENCES_KEY, 0);
+                return preferences.getInt("ui_homescreen_general_grid_row_count", 4);
+            }
+            public static int getGridColumnCount(Context context) {
+                final SharedPreferences preferences = context.getSharedPreferences(PREFERENCES_KEY, 0);
+                return preferences.getInt("ui_homescreen_general_grid_column", 4);
+            }
         }
 
         public static class Drawer {
