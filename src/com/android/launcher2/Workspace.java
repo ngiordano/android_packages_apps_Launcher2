3703fdde5e44fbbc6a97fc4e645b8a47d19b7ff7
nebkat <nebkat@teamhacksung.org>
2012-03-28 20:26:54 +0100

    4.0.4 merge
    Change-Id: I9f7e3fb806d2aa9a95e9f125d05fc31bc20654bd

--- src/com/android/launcher2/Workspace.java
+++ src/com/android/launcher2/Workspace.java
@@ -2034,7 +2034,7 @@ public class Workspace extends PagedView
                     invalidate();
                     for (int i = 0; i < getChildCount(); i++) {
                         final CellLayout cl = (CellLayout) getPageAt(i);
-                        cl.fastInvalidate();
+                        cl.invalidate();
                         cl.setFastTranslationX(a * mOldTranslationXs[i] + b * mNewTranslationXs[i]);
                         cl.setFastTranslationY(a * mOldTranslationYs[i] + b * mNewTranslationYs[i]);
                         cl.setFastScaleX(a * mOldScaleXs[i] + b * mNewScaleXs[i]);
@@ -2044,6 +2044,7 @@ public class Workspace extends PagedView
                         cl.setBackgroundAlphaMultiplier(a * mOldBackgroundAlphaMultipliers[i] +
                                 b * mNewBackgroundAlphaMultipliers[i]);
                         cl.setFastAlpha(a * mOldAlphas[i] + b * mNewAlphas[i]);
+                        cl.invalidate();
                     }
                     syncChildrenLayersEnabledOnVisiblePages();
                 }
